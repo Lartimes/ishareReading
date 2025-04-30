9768dec13fbe44c4e468eaacdafc29ab764af301
茜雫 <lartimes2004@163.com>
2025-04-30 08:52:20 +0800

    type

--- backend-reading/src/main/java/org/ishareReading/bankai/controller/TypeController.java
+++ backend-reading/src/main/java/org/ishareReading/bankai/controller/TypeController.java
@@ -1,6 +1,8 @@
 package org.ishareReading.bankai.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import org.ishareReading.bankai.model.Types;
 import org.ishareReading.bankai.response.Response;
 import org.ishareReading.bankai.service.TypesService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
  * 类型相关的，只是CRUD，不过四种类型都放到一个表里
  * 还是做个controller吧
  * 当然，aop也可以，不过显得有点多余了，类更多了
+ * ======================================
+ * 类型：见解、书籍、评论、帖子
  */
 @RestController
 @RequestMapping("/type")
 public class TypeController {
+
+//    opinion <<<<
+//    comment<<<
+//    ==================这俩没必要放
+//    book
+//    post
 
     private final TypesService typesService;
 
@@ -35,7 +45,8 @@ public class TypeController {
      * 根据一大类Type 获取types
      */
     public Response getTypeByType(String type) {
-        return null;
+        return Response.success(typesService.list(new LambdaQueryWrapper<Types>()
+                .eq(Types::getType, type)));
     }
 //    还要做什么呢？
 
